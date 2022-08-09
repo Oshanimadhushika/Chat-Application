0716c5302c5c28f4279b6aa943e72d3f3ba6632b
oshani madhushika <oshanimadhushika12@gmail.com>
2022-08-09 20:07:19 +0530

    Create Client 2

--- Chat Application/src/Client_02.java
+++ Chat Application/src/Client_02.java
@@ -1,2 +1,22 @@
-public class Client_02 {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class Client_02 extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/UserLoginForm.fxml"))));
+        primaryStage.setResizable(false);
+        //primaryStage.getIcons().add(new Image("location"));
+        primaryStage.setTitle("sample title");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
 }
